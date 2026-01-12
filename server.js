8d08a9c19f531609345c412ab58199172192168b
Rosyad Wafa <89338910+sabiliwafa29@users.noreply.github.com>
2026-01-12 18:49:29 +0700

    Switch MySQL driver from 'mysql' to 'mysql2'

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require('mysql2');
 const session = require("express-session");
 const path = require("path");
 const cors = require('cors');
